821775f628303e3f059cda981600f99b7663a5cb
Babul Kumar <BabulKumar100@users.noreply.github.com>
2025-08-04 18:52:55 +0530

    Added root route to confirm backend is running

--- backend/Server.js
+++ backend/Server.js
@@ -14,6 +14,11 @@ app.use('/api/auth', require('./routes/authRoutes'));
 
 app.use('/api/profile', require('./routes/Profile'));
 
+app.get("/", (req, res) => {
+  res.send("Welcome to Blinkit Backend");
+});
+
+
 const PORT= process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on Port ${PORT}`) );
 
